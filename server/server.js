10076546fccc6918c0a23c855d4f541dcc850d81
Jeremy <jeremyxu123@gmail.com>
2018-03-21 13:50:29 -0700

    add route and use postan

--- server/server.js
+++ server/server.js
@@ -15,12 +15,53 @@ app.use(bodyParser.json());
 // })
 
 app.post('/todos', (req, res) => {
-  console.log(req.body.completed);
+  const todo = new Todo({
+    text: req.body.text,
+    completed: req.body.completed
+  })
+  todo.save().then(doc => {
+    res.send(doc);
+  }, e => {
+    res.send(e);
+  })
 })
+
+//reading all
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({todos})
+    }, e => {
+      res.status(404).send(e);
+    })
+})
+
+app.get('todos/:id', (req, res) => {
+  //5ab2bbd3151f600bc62b5bec
+  Todo.findById("5ab2bbd3151f600bc62b5bec");
+})
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 })
+
+Chrome:
+/todos/123/Jeremy?school=paly
+Route:
+/todos/:id/:name
+rec.params.id; => '123'
+rec.params.name; => 'Jeremy'
+rec.query.school; => 'paly'
+
+-----------------------------
+
+Chrome:
+/todos?school=paly&num=5
+Route:
+/todos
+rec.query.school; => 'paly'
+rec.query.num; => '5'
 
 
 // const todoSchema = mongoose.Schema({
