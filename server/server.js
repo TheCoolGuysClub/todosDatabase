4b715830a294c4184100f344c3ebe20f2207b1c6
Jeremy <jeremyxu123@gmail.com>
2018-03-26 14:24:27 -0700

    add crud ops and forms

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
+//downloaded
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
+const hbs = require('hbs');
+const path = require('path');
 
+//local
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '../views'));
+
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 // app.use((req, res, next) => {
 //   req.body = {};
@@ -15,6 +25,7 @@ app.use(bodyParser.json());
 // })
 
 app.post('/todos', (req, res) => {
+  console.log(req.body);
   const todo = new Todo({
     text: req.body.text,
     completed: req.body.completed
@@ -36,9 +47,57 @@ app.get('/todos', (req, res) => {
     })
 })
 
-app.get('todos/:id', (req, res) => {
+app.get('/todos/new', (req, res) => {
+  res.render('./todos/new.hbs');
+})
+
+app.get('/todos/:id/edit', (req, res) => {
+  
+})
+
+
+app.get('/todos/:id', (req, res) => {
   //5ab2bbd3151f600bc62b5bec
-  Todo.findById("5ab2bbd3151f600bc62b5bec");
+  const id = req.params.id;
+  Todo.findById(id)
+    .then(todo => {
+      res.send({todo})
+    }, e=> {
+      res.status(404).send(e);
+    })
+})
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  Todo.findByIdAndRemove(id)
+    .then(todo => {
+      res.send({todo})
+    }, e => {
+      res.status(404).send(e);
+    })
+})
+
+app.patch('todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then(todo => {
+      if (!todo) {
+        res.status(404).send()
+      } else {
+        res.send(todo);
+      }
+
+    }, e => {
+      res.status(404).send(e);
+    })
 })
 
 
